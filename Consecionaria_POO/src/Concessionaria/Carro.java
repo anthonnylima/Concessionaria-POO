02c5b26fcb5787fae0f2b5d80a684275d061ad8e
antho <antho@Anthonny>
2026-06-08 14:52:32 -0300

    alteraçoes no codigo e finalização

--- Consecionaria_POO/src/Concessionaria/Carro.java
+++ Consecionaria_POO/src/Concessionaria/Carro.java
@@ -6,12 +6,24 @@ public class Carro {
     String marca;
     double valor;
     String placa;
+    String tipo;
 
-    public Carro(String modelo, int anoCarro, String marca, double valor, String placa) {
+    public Carro(String modelo, int anoCarro, String marca, double valor, String placa, String tipo) {
         this.modelo = modelo;
         this.anoCarro = anoCarro;
         this.marca = marca;
         this.valor = valor;
         this.placa = placa;
+        this.tipo = tipo;
     }
+
+    @Override
+    public String toString() {
+        return "Modelo: " + modelo +
+            "\nMarca: " + marca +
+            "\nAno do carro: " + anoCarro +
+            "\nValor: " + valor +
+            "\nPlaca: " + placa +
+            "\nTipo: " + tipo;
+	}
 }
