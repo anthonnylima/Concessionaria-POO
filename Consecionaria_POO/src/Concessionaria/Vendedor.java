09db7c1d7b5028c4e8482654443633fa33a83a68
anthonnylima <anthonnylima17@gmail.com>
2026-06-08 12:34:15 -0300

    Update Concessionaria.java

--- Consecionaria_POO/src/Concessionaria/Vendedor.java
+++ Consecionaria_POO/src/Concessionaria/Vendedor.java
@@ -1,23 +1,38 @@
 package Concessionaria;
-
 import java.util.ArrayList;
 
-public class Vendedor {
-    String nome;
-    ArrayList<Carro> carros;
-
-    public Vendedor(String nome) {
-        this.nome = nome;
-        this.carros = new ArrayList<>();
-    }
-
-    public void adicionarCarro(Carro carro) {
-        carros.add(carro);
-    }
-
-    public void listarCarros() {
-        for (Carro carro : carros) {
-            System.out.println(carro.modelo);
-        }
-    }
+public class concessionaria {
+	
+	ArrayList<Carro> carros;
+	
+	public concessionaria() {
+		this.carros = new ArrayList<>();
+	}
+	
+	public void adicionarCarro(Carro carro) {
+		carros.add(carro);
+	}
+	
+	public void listarCarros() {
+		for(Carro carro : carros) {
+			System.out.println(carro);
+		}
+	}
+	
+	public void buscarPlaca(String placaProcurada) {
+		for(Carro carro : carros) {
+			if(carro.placa.equals(placaProcurada)) {
+				System.out.println(carro);
+			}
+		}
+	}
+	
+	public void removerPlaca(String placaProcurada) {
+		for(int i = 0; i < carros.size(); i++) {
+			if(carros.get(i).placa.equals(placaProcurada)) {
+				carros.remove(i);
+				break;
+			}
+		}
+	}
 }
