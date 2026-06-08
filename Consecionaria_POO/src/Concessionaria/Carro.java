0adfaa93201ba56757713b70c9f0200fccf455d7
anthonnylima <anthonnylima17@gmail.com>
2026-06-08 12:31:44 -0300

    Update Carro.java
    Update da classe carro adcionando to string

--- Consecionaria_POO/src/Concessionaria/Carro.java
+++ Consecionaria_POO/src/Concessionaria/Carro.java
@@ -2,16 +2,28 @@ package Concessionaria;
 
 public class Carro {
     String modelo;
-    int anoCarro;
     String marca;
+    int anoCarro;
     double valor;
     String placa;
+    String tipo;
 
-    public Carro(String modelo, int anoCarro, String marca, double valor, String placa) {
+    public Carro(String modelo, int anoCarro, String marca, double valor, String placa, String tipo) {
         this.modelo = modelo;
         this.anoCarro = anoCarro;
         this.marca = marca;
         this.valor = valor;
         this.placa = placa;
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Modelo: " + modelo +
+            "\nMarca: " + marca +
+            "\nAno do carro: " + anoCarro +
+            "\nValor: " + valor +
+            "\nPlaca: " + placa +
+            "\nTipo: " + tipo;
     }
 }
